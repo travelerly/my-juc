6c21a9afda4a2c65f0a0f32011fd8b98c9fa2977
liyang <yang.li@beantechs.com>
2021-03-12 12:05:59 +0800

    first commit

---
+++ src/main/java/com/colin/MyJucApplication.java
@@ -0,0 +1,13 @@
+package com.colin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJucApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/colin/MyJucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
